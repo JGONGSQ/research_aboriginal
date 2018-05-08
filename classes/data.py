ca9ce40e07a2fbf2f2a9561239a4f6e5d5d195b1
James <james.gong927@gmail.com>
2018-05-08 16:57:12 +1000

    Minor changes

--- classes/data.py
+++ classes/data.py
@@ -1,25 +1,56 @@
 #!/usr/bin/python
 
-# packages
+# local packages
 from .settings import *
 
 # python imports
+import csv
 
 
-class Data(object):
+class DataProcessor(object):
 
-    def __init__(self):
-        self.data_file = DEFAULT_DATA_FILE_CSV
+    filtered_data_filepath = FILTERED_DATA_FILE_CSV
+    processed_data_filepath = PROCESSED_DATA_FILE_CSV
 
-    # read
+    # def __init__(self):
+    #     print(123)
 
-    # filter
+    def read_csv(self, filepath):
 
-    # write
+        data = list()
+
+        with open(filepath, 'rU') as csvfile:
+            file_reader = csv.reader(csvfile, delimiter=',')
+
+            for row in file_reader:
+                data.append(row)
+
+        return data
+
+    def write_csv(self, filepath, data):
+        """
+            Write the results list to generate a new data file
+        :param filename: the name of the output file with its path
+        :param data: its a two-dimensional array
+        :return: True if success
+        """
+
+        # open the file need to be write
+        with open(filepath, 'w') as csvfile:
+            # initial the writer
+            writer = csv.writer(csvfile, delimiter=',')
+
+            # write each row
+            for row in data:
+                writer.writerow(row)
+
+        return
+
+    def modify_the_data(self, input_filepath, output_filepath):
+        print(123)
+        return
 
 
 
-
-print("testing line")
 
 
